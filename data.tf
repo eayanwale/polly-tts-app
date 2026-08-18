126830f1e907ee8ebb45f1018115a0fa30880cb6
Enoch Ayanwale <enochayanwale@outlook.com>
2026-08-18 11:54:32 -0400

    Add data sources for Route53 zone, ACM cert, and CloudFront bucket policy
    - aws_route53_zone lookup for the root domain
    - aws_acm_certificate lookup for *.clixx.example.com
    - aws_iam_policy_document granting the CloudFront OAC read/write on
      the website bucket, replacing the old public-read policy

---
+++ data.tf
@@ -0,0 +1,55 @@
+data "aws_route53_zone" "root-domain" {
+  name         = "clixx.example.com"
+  private_zone = false
+}
+
+data "aws_iam_policy_document" "origin_bucket_policy" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.website_bucket.arn}/*",
+    ]
+  }
+
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalReadWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.website_bucket.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
+
+data "aws_acm_certificate" "my_domain" {
+  region   = "us-east-1"
+  domain   = "*.${local.my_domain}"
+  statuses = ["ISSUED"]
+}
